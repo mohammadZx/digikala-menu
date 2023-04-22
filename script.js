d308658d16f8beb403b6baf62872f950d15efa2e
یادگیری بهتر برنامه نویسی اموزش رایگان برنامه نویسی <mohammad13yazdani@gmail.com>
2023-04-23 00:24:41 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$('.search-submit').on('click', function(e){
+    if(window.innerWidth <= 720){
+        e.preventDefault()
+    }
+    $('.search-input').toggleClass('active')
+})
+
+$('.nav-menu .toggle, .menu-section .menu li.close').on('click', function(e){
+    $('.nav-menu .menu').toggleClass('active')
+})
+
+$('.mega-menu-box ul.menu-menu > li').on('mouseover', function(){
+    if(window.innerWidth > 720){
+    $('.menucontent').css({display: 'none'})
+    $(this).find('.menucontent').css({display: 'inline-block'})
+    }
+})
+
+$('li.mega-menu-item .menucontent ul.items > li a').on('click', function(e){
+    if(window.innerWidth <= 720){
+    e.preventDefault()
+    $(this).parent().toggleClass('active')
+    console.log(this)
+
+    }
+})
+
+$('li.mega-menu-item > a').on('click', function(e){
+    if(window.innerWidth <= 720){
+    e.preventDefault()
+    $(this).parent().toggleClass('active')
+    console.log(this)
+    }
+})
+
+
